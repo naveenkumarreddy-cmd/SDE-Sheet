1c6c4b9afc86f22d173a1941fe1df6fb853c8108
naveenkumarreddy-cmd <naveenreddy7919@gmail.com>
2026-06-01 12:39:43 +0530

    Day-1 coding problems

---
+++ DAY-1/src/PascalTriangle.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class PascalTriangle {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows in pascal triangle");
+        int n = sc.nextInt();
+
+        List<List<Integer>> result = triangle(n);
+
+        for(int i=0;i<n;i++) {
+            System.out.println(result.get(i));
+        }
+
+
+    }
+
+    public static int ncr(int row,int column) {
+        row = row -1;
+        column = column - 1;
+        int result = 1;
+        for(int i=0;i<column;i++) {
+            result = result * (row-i);
+            result = result /(i+1);
+        }
+        return result;
+    }
+
+    public static List<Integer> printRow(int row) {
+        List<Integer> list = new ArrayList<>();
+        //First element in row is 1
+       int answer = 1;
+       list.add(answer);
+
+       for(int col=1;col<row;col++) {
+           answer = answer * (row-col);
+           answer = answer/col;
+           list.add(answer);
+       }
+        return list;
+    }
+
+    public static List<List<Integer>> triangle(int n) {
+        List<List<Integer>> answer = new ArrayList<List<Integer>>();
+
+        for(int i=1;i<=n;i++) {
+            answer.add(printRow(i));
+        }
+        return answer;
+    }
+}
